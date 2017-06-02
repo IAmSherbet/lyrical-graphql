5074174992ef1e64f52a828e5c211e8339bf7f91
sbajracharya <sbajracharya@trioxis.com>
2017-06-02 20:54:39 +1000

    Add SongList component

---
+++ client/components/SongList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+
+class SongList extends Component {
+  render() {
+    return (
+      <div>
+        SongList
+      </div>
+    );
+  }
+}
+
+
+
+export default SongList;
