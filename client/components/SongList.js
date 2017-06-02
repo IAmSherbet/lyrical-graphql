b14aa182c7f529c1d30834da665435ec7c61dcd6
sbajracharya <sbajracharya@trioxis.com>
2017-06-02 21:18:53 +1000

    Bind GraphQL query with SongList component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={ song.id } className="collection-item">
+          { song.title }
+        </li>
+      );
+    });
+  }
+
   render() {
-    return (
-      <div>
-        SongList
-      </div>
-    );
+    if (this.props.data.loading) {
+      return <div>Loading ... </div>
+    } else {
+      return (
+        <ul className="collection">
+          { this.renderSongs() }
+        </ul>
+      );
+    }
   }
 }
 
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
 
-
-export default SongList;
+export default graphql(query)(SongList);
+// This is valid javascript, it calls a function with query as the
+// first argument then runs a second time once the query completes
+// with SongList as an argument
