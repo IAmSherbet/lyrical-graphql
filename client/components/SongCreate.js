e4572c463ead516972ed9cdd5593fcd7040d335a
sbajracharya <sbajracharya@trioxis.com>
2017-06-04 19:25:35 +1000

    redirect user and refetch songs after songCreate mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
+import fetchSongs from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -12,15 +14,15 @@ class SongCreate extends Component {
     event.preventDefault();
 
     this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
-    });
+      variables: { title: this.state.title },
+      refetchQueries: [{ query: fetchSongs }],
+    }).then(() => hashHistory.push("/"))
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h4>Create a New Song</h4>
         <form onSubmit={ this.onSubmit.bind(this) }>
           <label>Song Title</label>
