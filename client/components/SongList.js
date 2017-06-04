3b45e6090061e2af9a2ae099c83a450fe43458fc
sbajracharya <sbajracharya@trioxis.com>
2017-06-04 19:28:11 +1000

    Add delete mutation to song list

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ title, id }) => {
       return (
-        <li key={ song.id } className="collection-item">
-          { song.title }
+        <li key={ id } className="collection-item">
+          { title }
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(id)}
+          >
+            delete
+          </i>
         </li>
       );
     });
@@ -18,24 +31,34 @@ class SongList extends Component {
       return <div>Loading ... </div>
     } else {
       return (
-        <ul className="collection">
-          { this.renderSongs() }
-        </ul>
+        <div>
+          <ul className="collection">
+            { this.renderSongs() }
+          </ul>
+          <Link
+            to="/songs/new"
+            className="btn-floating btn large red right"
+          >
+            <i className="material-icons">add</i>
+          </Link>
+        </div>
       );
     }
   }
 }
 
-const query = gql`
-  {
-    songs {
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
       id
-      title
     }
   }
 `;
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
+
 // This is valid javascript, it calls a function with query as the
 // first argument then runs a second time once the query completes
 // with SongList as an argument
